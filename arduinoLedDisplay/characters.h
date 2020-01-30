e2f4964133e65fa27fd74992b6cb327e73a26d66
Maxime Chretien (MixLeNain) <mchretien@linuxmail.org>
2020-01-30 16:37:55 +0100

    characters.h: Add characters struct and array

--- arduinoLedDisplay/characters.h
+++ arduinoLedDisplay/characters.h
@@ -1,6 +1,13 @@
 #ifndef CHARACTERS_H
 #define CHARACTERS_H
 
+#define NBCHAR 50
 
+struct characters {
+	uint8_t length;
+	uint8_t pixels[5];
+};
+
+const struct characters chars[NBCHAR];
 
 #endif
