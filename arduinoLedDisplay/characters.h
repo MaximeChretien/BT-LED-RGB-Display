8207ae750c93c4a799bad33eb296bd900a1c5e01
Maxime Chretien (MixLeNain) <mchretien@linuxmail.org>
2020-01-30 19:38:25 +0100

    characters.h: multiple changes
    - Add numbers
    - Rename struct
    - Add character decoding function
    - Add comments

--- arduinoLedDisplay/characters.h
+++ arduinoLedDisplay/characters.h
@@ -2,19 +2,38 @@
 // Description : define the char struct and the way to show each char
 // 		using the led display (pixels repartition)
 // Authors : Maxime Chretien (MixLeNain)
-// Version : 0.1
+// Version : 0.2
 
 #ifndef CHARACTERS_H
 #define CHARACTERS_H
 
-#define NBCHAR 26
+#define NBCHAR 26+10
 
-struct characters {
-	uint8_t length;
-	uint8_t pixels[5];
+//character struct
+struct character {
+	uint8_t length; //number of bit used
+	uint8_t pixels[5]; //used to store bits
 };
 
-const struct characters chars[NBCHAR] = {
+//Return the correct struct corresponding to the given char
+struct characters getCharacter(char symbol) {
+	struct character result;
+
+	symbol = tolower(symbol); //lowercase because we don't support case
+	
+	if (symbol >= 'a' && symbol < 'z') {
+		result = chars[symbol - 'a'];
+
+	} else if (symbol >= '0' && symbol < '9') {
+		result = chars[26 + (symbol - '0')];
+	} else {
+		result = {4, {0,0,0,0,0}};
+	}
+
+	return result;
+}
+
+const struct character chars[NBCHAR] = {
 	//Letter A
 	{4, {	0b00000110,
 		0b00001001,
@@ -196,6 +215,76 @@ const struct characters chars[NBCHAR] = {
 		0b00000010,
 		0b00000100,
 		0b00001111}
+	},
+	//Number 0
+	{4, {	0b00000110,
+		0b00001001,
+		0b00001001,
+		0b00001001,
+		0b00000110}
+	},
+	//Number 1
+	{3, {	0b00000010,
+		0b00000110,
+		0b00000010,
+		0b00000010,
+		0b00000111}
+	},
+	//Number 2
+	{4, {	0b00000110,
+		0b00001001,
+		0b00000011,
+		0b00000100,
+		0b00001111}
+	},
+	//Number 3
+	{4, {	0b00001110,
+		0b00000001,
+		0b00000111,
+		0b00000001,
+		0b00001110}
+	},
+	//Number 4
+	{4, {	0b00001000,
+		0b00001010,
+		0b00000111,
+		0b00000010,
+		0b00000010}
+	},
+	//Number 5
+	{4, {	0b00001111,
+		0b00001000,
+		0b00001111,
+		0b00000001,
+		0b00001111}
+	},
+	//Number 6
+	{4, {	0b00000111,
+		0b00001000,
+		0b00001110,
+		0b00001001,
+		0b00000110}
+	},
+	//Number 7
+	{4, {	0b00000110,
+		0b00001001,
+		0b00000010,
+		0b00000100,
+		0b00000100}
+	},
+	//Number 8
+	{4, {	0b00000110,
+		0b00001001,
+		0b00000110,
+		0b00001001,
+		0b00000110}
+	},
+	//Number 9
+	{4, {	0b00000110,
+		0b00001001,
+		0b00000111,
+		0b00000001,
+		0b00000110}
 	}
 };
 
